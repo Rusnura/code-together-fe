9f2c95751f13d0a781d24d26f53b3afd0ac03ad2
Ruslan Tumasoff <Rus-nura@yandex.ru>
2022-08-17 00:08:36 +0700

    Improvements: + DebugComponent.svelte using cursors array instead of object and added button for put textarea info to console

--- src/components/DebugComponent.svelte
+++ src/components/DebugComponent.svelte
@@ -1,21 +1,21 @@
 <script>
-    let cursors;
-
     export let textareaProperties;
-    $: {
-        cursors = Object.entries(textareaProperties.cursors);
-        console.log("DebugComponent: cursors entries: ", cursors);
-    }
     export let cursorInfo;
     export let sessionInfo;
+
+    let putTextareaPropertiesToConsole = function () {
+        console.log("DebugComponent: cursors entries: ", textareaProperties.cursors);
+    }
 </script>
 
 <main>
     <h3>Отладочная информация:</h3>
     Курсор пользователя: {sessionInfo.username}: <i>{cursorInfo.startCursorPosition}/{cursorInfo.endCursorPosition}</i><br>
-    {#each cursors as [user, cursor]}
-        Курсор пользователя: {user}: <i>{cursor.startCursorPosition}/{cursor.endCursorPosition}</i><br>
+    {#each textareaProperties.cursors as cursor, i}
+        Курсор пользователя: {cursor.owner} ({i}): <i>{cursor.startCursorPosition}/{cursor.endCursorPosition}</i><br>
     {/each}
+    <br>
+    <button on:click="{putTextareaPropertiesToConsole}">Put textarea properties to console</button>
 </main>
 
 <style>
